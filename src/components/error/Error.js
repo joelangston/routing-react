f053fa73c0ee257ebd0d0003b90163ffa0eb2b74
Joe Langston <joelangston81@gmail.com>
2020-08-10 20:22:47 -0500

    almost done

---
+++ src/components/error/Error.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+function Error(props) {
+    return (
+        <div className="404">
+            <h1> This Page Does Not Exist.</h1>
+        </div>
+    );
+}
+export default Error;
